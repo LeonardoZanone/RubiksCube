0029a8c4cc836aad706b0a149db22eb586748f37
160015 <160015@A1211.labinfo.facens.br>
2019-05-06 21:50:41 -0300

    Mapeamento das teclas que serão utilizadas.

--- Rubikscube/src/mygame/Main.java
+++ Rubikscube/src/mygame/Main.java
@@ -1,6 +1,9 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.math.Transform;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
@@ -22,6 +25,7 @@ public class Main extends SimpleApplication {
     }
     
     Node[] nodes = new Node[2];
+    private boolean isRunning = true;
 
     @Override
     public void simpleInitApp() {
@@ -54,4 +58,70 @@ public class Main extends SimpleApplication {
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
     }
+    
+    private void initKeys() {
+        inputManager.addMapping("E", new KeyTrigger(KeyInput.KEY_E)); //Move a parte de cima para a esquerda
+        inputManager.addMapping("R", new KeyTrigger(KeyInput.KEY_R)); //Move a parte de cima para a direita
+        inputManager.addMapping("D", new KeyTrigger(KeyInput.KEY_D)); //Move a parte de baixo para a esquerda
+        inputManager.addMapping("F", new KeyTrigger(KeyInput.KEY_F)); //Move a parte de baixo para a direita
+        inputManager.addMapping("I", new KeyTrigger(KeyInput.KEY_I)); //Move a parte da esquerda para cima
+        inputManager.addMapping("K", new KeyTrigger(KeyInput.KEY_K)); //Move a parte da esquerda para baixo
+        inputManager.addMapping("O", new KeyTrigger(KeyInput.KEY_O)); //Move a parte da direita para cima
+        inputManager.addMapping("L", new KeyTrigger(KeyInput.KEY_L)); //Move a parte da direita para baixo
+        
+        inputManager.addMapping("Direita", new KeyTrigger(KeyInput.KEY_RIGHT)); //Rotaciona o cubo para a direita
+        inputManager.addMapping("Esquerda", new KeyTrigger(KeyInput.KEY_LEFT)); //Rotaciona o cubo para a esquerda
+        inputManager.addMapping("Cima", new KeyTrigger(KeyInput.KEY_UP)); //Rotaciona o cubo para cima
+        inputManager.addMapping("Baixo", new KeyTrigger(KeyInput.KEY_DOWN)); //Rotaciona o cubo para baixo
+        
+        inputManager.addListener(actionListener, "E", "R", "D", "F", "I", "K", "O", "L", "Direita", "Esquerda", "Cima", "Baixo");
+    }
+    
+    private final ActionListener actionListener = new ActionListener() {
+        @Override
+        public void onAction(String name, boolean keyPressed, float tpf) {
+            if (isRunning && !keyPressed) {
+                switch(name){
+                    case "E":
+                        
+                        break;
+                    case "R":
+                        
+                        break;
+                    case "D":
+                        
+                        break;
+                    case "F":
+                        
+                        break;
+                    case "I":
+                        
+                        break;
+                    case "K":
+                        
+                        break;
+                    case "O":
+                        
+                        break;
+                    case "L":
+                        
+                        break;
+                    case "Direita":
+                        
+                        break;
+                    case "Esquerda":
+                        
+                        break;
+                    case "Cima":
+                        
+                        break;
+                    case "Baixo":
+                        
+                        break;
+                }
+                
+            }
+        }
+    };
+
 }
